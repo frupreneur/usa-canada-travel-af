9cb0ac84f7fc383a8c76c5c79dc86a6f4fc0e318
frupreneur <frupreneur@gmail.com>
2024-04-06 20:30:16 +0100

    committed

--- db/visa.ts
+++ db/visa.ts
@@ -1,126 +1,88 @@
-const DATABASE = [{
-  "route": "usa-jobs-visa-requirements",
-  "title": "Jobs in USA: Visa Requirements for Immigrants",
-  "description": "Discover the visa requirements for immigrants interested in working in the USA and assess your preparedness for the application process.",
-  "result": 0,
-  "idx": 0,
-  "high": "Based on your responses, it seems you're well-prepared to explore job opportunities in the USA. Consider consulting with immigration professionals for detailed guidance on visa procedures.",
-  "low": "Your answers indicate that you may need to enhance your readiness before pursuing job opportunities in the USA. Conduct thorough research on visa requirements and employment prospects to make informed decisions.",
-  "questions": [
-    {
-      "question": "Do you have a clear understanding of the visa categories available for working in the USA?",
-      "answers": ["Yes", "No", "Somewhat"],
-      "correct": "Yes"
-    },
-    {
-      "question": "Have you evaluated your eligibility for different types of U.S. work visas?",
-      "answers": ["Yes", "No", "Partially"],
-      "correct": "Yes"
-    },
-    {
-      "question": "Are you aware of the documentation required for a work visa application?",
-      "answers": ["Yes", "No", "Somewhat"],
-      "correct": "Yes"
-    },
-    {
-      "question": "Have you considered the potential timeframes for obtaining a work visa?",
-      "answers": ["Yes", "No", "Not Yet"],
-      "correct": "Yes"
-    },
-    {
-      "question": "Do you understand the financial implications of obtaining a work visa?",
-      "answers": ["Yes", "No", "To Some Extent"],
-      "correct": "Yes"
-    },
-    {
-      "question": "Have you researched the job market and demand for your skills in the USA?",
-      "answers": ["Yes", "No", "In Progress"],
-      "correct": "Yes"
-    },
-    {
-      "question": "Are you familiar with the process of obtaining a job offer from a U.S. employer?",
-      "answers": ["Yes", "No", "Somewhat"],
-      "correct": "Yes"
-    },
-    {
-      "question": "Have you explored potential relocation challenges and solutions?",
-      "answers": ["Yes", "No", "Not Yet"],
-      "correct": "Yes"
-    },
-    {
-      "question": "Do you understand the implications of U.S. taxation laws on your income?",
-      "answers": ["Yes", "No", "To Some Extent"],
-      "correct": "Yes"
-    },
-    {
-      "question": "Have you assessed the cultural and lifestyle differences between your home country and the USA?",
-      "answers": ["Yes", "No", "Somewhat"],
-      "correct": "Yes"
-    }
-  ]
-}, {
-  "route": "canada-jobs-visa-requirements",
-  "title": "Jobs in Canada: Visa Requirements for Immigrants",
-  "description": "Explore the visa requirements for immigrants seeking employment in Canada and assess your readiness for the application process.",
-  "result": 0,
-  "idx": 0,
-  "high": "Based on your responses, it appears you may be well-prepared to explore job opportunities in Canada. It's advisable to consult with immigration experts to understand the visa procedures thoroughly.",
-  "low": "Your answers suggest that you may need to further prepare before considering job opportunities in Canada. Research and gather necessary information about visa requirements and job prospects before proceeding.",
-  "questions": [
-    {
-      "question": "Do you have a clear understanding of the visa categories available for working in Canada?",
-      "answers": ["Yes", "No", "Somewhat"],
-      "correct": "Yes"
-    },
-    {
-      "question": "Have you evaluated your eligibility for different types of Canadian work permits?",
-      "answers": ["Yes", "No", "Partially"],
-      "correct": "Yes"
-    },
-    {
-      "question": "Are you aware of the documentation required for a Canadian work permit application?",
-      "answers": ["Yes", "No", "Somewhat"],
-      "correct": "Yes"
-    },
-    {
-      "question": "Have you considered the potential processing times for obtaining a Canadian work permit?",
-      "answers": ["Yes", "No", "Not Yet"],
-      "correct": "Yes"
-    },
-    {
-      "question": "Do you understand the financial requirements associated with obtaining a Canadian work permit?",
-      "answers": ["Yes", "No", "To Some Extent"],
-      "correct": "Yes"
-    },
-    {
-      "question": "Have you researched the job market and demand for your skills in Canada?",
-      "answers": ["Yes", "No", "In Progress"],
-      "correct": "Yes"
-    },
-    {
-      "question": "Are you familiar with the process of obtaining a job offer from a Canadian employer?",
-      "answers": ["Yes", "No", "Somewhat"],
-      "correct": "Yes"
-    },
-    {
-      "question": "Have you explored potential settlement challenges and solutions in Canada?",
-      "answers": ["Yes", "No", "Not Yet"],
-      "correct": "Yes"
-    },
-    {
-      "question": "Do you understand the implications of Canadian taxation laws on your income?",
-      "answers": ["Yes", "No", "To Some Extent"],
-      "correct": "Yes"
-    },
-    {
-      "question": "Have you assessed the cultural and lifestyle differences between your home country and Canada?",
-      "answers": ["Yes", "No", "Somewhat"],
-      "correct": "Yes"
-    }
-  ]
-}
-
+const DATABASE = [
+	{
+		route: "usa-jobs-visa-requirements",
+		title: "Jobs in USA: Visa Requirements for Immigrants",
+		description:
+			"Discover the visa requirements for immigrants interested in working in the USA and assess your preparedness for the application process.",
+		result: 0,
+		idx: 0,
+		high: "Based on your responses, it seems you're well-prepared to explore job opportunities in the USA. Consider consulting with immigration professionals for detailed guidance on visa procedures.",
+		low: "Your answers indicate that you may need to enhance your readiness before pursuing job opportunities in the USA. Conduct thorough research on visa requirements and employment prospects to make informed decisions.",
+		questions: [
+			{
+				question:
+					"Do you have a clear understanding of the visa categories available for working in the USA?",
+				answers: ["Yes", "No", "Somewhat"],
+				correct: "Yes",
+			},
+			{
+				question:
+					"Have you evaluated your eligibility for different types of U.S. work visas?",
+				answers: ["Yes", "No", "Partially"],
+				correct: "Yes",
+			},
+			{
+				question:
+					"Are you aware of the documentation required for a work visa application?",
+				answers: ["Yes", "No", "Somewhat"],
+				correct: "Yes",
+			},
+			{
+				question:
+					"Have you considered the potential timeframes for obtaining a work visa?",
+				answers: ["Yes", "No", "Not Yet"],
+				correct: "Yes",
+			},
+			{
+				question:
+					"Do you understand the financial implications of obtaining a work visa?",
+				answers: ["Yes", "No", "To Some Extent"],
+				correct: "Yes",
+			},
+		],
+	},
+	{
+		route: "canada-jobs-visa-requirements",
+		title: "Jobs in Canada: Visa Requirements for Immigrants",
+		description:
+			"Explore the visa requirements for immigrants seeking employment in Canada and assess your readiness for the application process.",
+		result: 0,
+		idx: 0,
+		high: "Based on your responses, it appears you may be well-prepared to explore job opportunities in Canada. It's advisable to consult with immigration experts to understand the visa procedures thoroughly.",
+		low: "Your answers suggest that you may need to further prepare before considering job opportunities in Canada. Research and gather necessary information about visa requirements and job prospects before proceeding.",
+		questions: [
+			{
+				question:
+					"Do you have a clear understanding of the visa categories available for working in Canada?",
+				answers: ["Yes", "No", "Somewhat"],
+				correct: "Yes",
+			},
+			{
+				question:
+					"Have you evaluated your eligibility for different types of Canadian work permits?",
+				answers: ["Yes", "No", "Partially"],
+				correct: "Yes",
+			},
+			{
+				question:
+					"Are you aware of the documentation required for a Canadian work permit application?",
+				answers: ["Yes", "No", "Somewhat"],
+				correct: "Yes",
+			},
+			{
+				question:
+					"Have you considered the potential processing times for obtaining a Canadian work permit?",
+				answers: ["Yes", "No", "Not Yet"],
+				correct: "Yes",
+			},
+			{
+				question:
+					"Do you understand the financial requirements associated with obtaining a Canadian work permit?",
+				answers: ["Yes", "No", "To Some Extent"],
+				correct: "Yes",
+			},
+		],
+	},
 ];
-  
-  export default DATABASE;
-  
+
+export default DATABASE;
